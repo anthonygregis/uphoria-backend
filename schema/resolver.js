bab916fc96a40acbb1f6cb963f0bb6f8f91cd643
Anthony Gregis <anthony@thedevtron.com>
2020-09-06 11:50:45 -0500

    [Models/Mutations] Add in User and Video models, add in signup, login, updateUser, and deleteUser mutations

--- schema/resolver.js
+++ schema/resolver.js
@@ -7,44 +7,74 @@ const APP_SECRET = process.env.APP_SECRET
 const resolver = {
   Query: {
     user: {
-      description: "Return a user based off their ID",
-      resolve: () => {
-        //TODO
+      description: "Returns a user based off their ID",
+      resolve: async (_, {id}, context) => {
+        return await db.User.findById(id)
       }
+    },
+    users: {
+    	description: "Returns all users",
+	    resolve: async () => {
+				return await db.User.find()
+	    }
     }
   },
   Mutation: {
-    signup: async (_, args, context) => {
-      const hashedPassword = await bcrypt.hash(args.password, 10)
-      args.password = hashedPassword
-      const newUser = await db.user.create({...args})
-      const user = newUser.dataValues
+    signup: {
+    	description: "Create a new user",
+	    resolve: async (_, args, context) => {
+		    args.password = await bcrypt.hash(args.password, 10)
+		    const newUser = await db.User.create({...args})
+		    console.log("New User Created:", newUser)
+		    const user = newUser
 
-      const token = jwt.sign({userId: newUser.id}, APP_SECRET)
+		    const token = jwt.sign({ userId: user._id }, APP_SECRET)
 
-      return {
-        token,
-        user,
-      }
+		    return {
+			    token,
+			    user,
+		    }
+	    }
     },
-    login: async (_, args, context) => {
-      const loginUser = await db.user.findOne({where: {username: args.username}})
-      if (!loginUser) {
-        throw new Error("No such user found")
-      }
+    login: {
+    	description: "Login and authenticate",
+	    resolve: async (_, args, context) => {
+		    const loginUser = await db.User.findOne({ email: args.email })
+		    if (!loginUser) {
+			    throw new Error("No such user found")
+		    }
 
-      const valid = await bcrypt.compare(args.password, loginUser.password)
-      if (!valid) {
-        throw new Error("Invalid password")
-      }
+		    const valid = await bcrypt.compare(args.password, loginUser.password)
+		    if (!valid) {
+			    throw new Error("Invalid password")
+		    }
 
-      const user = loginUser.dataValues
+		    const user = loginUser
 
-      return {
-        token: jwt.sign({...user}, APP_SECRET),
-        user,
-      }
+		    return {
+			    token: jwt.sign({...user}, APP_SECRET),
+			    user,
+		    }
+	    }
     },
+	  updateUser: {
+    	description: "Update a user",
+		  resolve: async (_, args, context) => {
+			  const { id, ...setArgs } = args
+			  return await db.User.findByIdAndUpdate({_id: id}, { $set: {...setArgs} }, { "new": true })
+		  }
+	  },
+	  deleteUser: {
+    	description: "We aren't Tik Tok",
+		  resolve: async (_, {id}, context) => {
+			  return new Promise((resolve, reject) => {
+				  const deletedUser = db.User.findByIdAndDelete(id, (err, docs) => {
+				  	if(err) reject(err)
+					  else resolve(true)
+				  })
+			  })
+		  }
+	  }
   }
 }
 
